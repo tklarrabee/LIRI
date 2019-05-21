6689da5377bd5c98af87c7d0290f0a7c42cfd53e
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-05-21 13:28:55 -0700

    movie-this functional

--- liri.js
+++ liri.js
@@ -1,1 +1,34 @@
-var 
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var axios = require("axios");
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var rawQuery = process.argv.splice(3);
+var query = rawQuery.join(" ");
+
+switch(command) {
+    case "movie-this":
+        axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                console.log(response.data);
+            }
+        );
+        break;
+
+    default:
+        console.log(`
+        LIRI Commands:
+        concert-this "<artist/band name>"
+        spotify-this-song "<song name>"
+        movie-this "<movie name>"`)
+}
+
+
+
+/* concert-this for bands in town
+spotify-this-song for spotify api
+movie-this for 
+do-what-it-says*/
